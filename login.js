65069d02dad1ee32743e6444c271a1e29e4f17f9
matifandy <matifandy@gmail.com>
2020-08-12 21:06:44 -0300

    second login

---
+++ login.js
@@ -0,0 +1,22 @@
+function validation(){
+    var email = document.getElementById('inputEmail').value;
+    var password = document.getElementById('inputPassword').value;
+    var error_message = document.getElementById('error_message');
+    var text;
+
+    error_message.style.padding = "10px";
+
+    if(email === ''){
+      text = "Escriba un email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(password === ''){
+      text = "Escriba una contraseña";
+      error_message.innerHTML = text;
+      return false;
+    }
+    
+    
+    return true;
+}
