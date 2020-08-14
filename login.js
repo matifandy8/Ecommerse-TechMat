a2197c0b3ac6aa26e5d1f2a3daa24d2254e4367e
matifandy <matifandy@gmail.com>
2020-08-13 22:14:38 -0300

    signin

--- login.js
+++ login.js
@@ -1,22 +1,31 @@
 function validation(){
-    var email = document.getElementById('inputEmail').value;
-    var password = document.getElementById('inputPassword').value;
-    var error_message = document.getElementById('error_message');
-    var text;
+  var email = document.getElementById('inputEmail').value;
+  var password = document.getElementById('inputPassword').value;
+  var error_message = document.getElementById('error_message');
+  var text;
 
-    error_message.style.padding = "10px";
+  error_message.style.padding = "10px";
 
-    if(email === ''){
-      text = "Escriba un email";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(password === ''){
-      text = "Escriba una contraseña";
-      error_message.innerHTML = text;
-      return false;
-    }
-    
-    
-    return true;
+  if(email === ''){
+    text = "Escriba un email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(password === ''){
+    text = "Escriba una contraseña";
+    error_message.innerHTML = text;
+    return false;
+  }
+  window.location="index.html"
+  return true;
 }
+
+// ------------------------
+
+const form = document.getElementById('myform');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    validation();
+});
