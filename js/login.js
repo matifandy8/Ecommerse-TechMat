141f1af6956242db1063d86ab9e7058507cc1861
matifandy <matifandy@gmail.com>
2020-09-12 15:58:54 -0300

    register page

--- js/login.js
+++ js/login.js
@@ -1,39 +1,24 @@
-function validation(){
-  var email = document.getElementById('inputEmail').value;
-  var password = document.getElementById('inputPassword').value;
-  var error_message = document.getElementById('error_message');
-  var text;
-
-  error_message.style.padding = "10px";
-
-  if(email === ''){
-    text = "Escriba un email";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(password === ''){
-    text = "Escriba una contraseña";
-    error_message.innerHTML = text;
-    return false;
-  }
-  window.location="home.html"
-  return true;
-}
 
 // ------------------------
 
 const form = document.getElementById('myform');
-const email = document.getElementById('inputEmail');
-const password = document.getElementById('inputPassword');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-let users = Array(
-        {
-          email: email.value,
-          contraseña: password.value
-        }
-);
-    localStorage.setItem('user',JSON.stringify(users));
-    validation();
+
+    const email = document.getElementById('inputEmail').value;
+    const password = document.getElementById('inputPassword').value;
+
+    
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+
+    //  clear the form
+    form.reset();
+
+
+      console.log('sign up')
+    })
+
 });
